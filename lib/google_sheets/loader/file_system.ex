9c714ef79d9cb1be9d08a53a268b807473bc5753
Endymio <janne.kaistinen@iki.fi>
2015-03-15 02:14:46 +0200

    Load only specified sheets from disk

--- lib/google_sheets/loader/file_system.ex
+++ lib/google_sheets/loader/file_system.ex
@@ -10,19 +10,19 @@ defmodule GoogleSheets.Loader.FileSystem do
   def load(sheets, _last_updated, config) when is_list(sheets) and is_list(config) do
     path = Path.expand Keyword.fetch!(config, :src)
     if File.exists? path do
-      files = Path.wildcard(path <> "/*.csv")
-      sheets = load_csv_files files, []
-      {nil, SpreadSheetData.new(path, sheets)}
+      files = Path.wildcard(path <> "/*.csv") |> Enum.filter(fn(filename) -> Path.basename(filename, ".csv") in sheets end)
+      loaded = load_csv_files files, []
+      {nil, SpreadSheetData.new(path, loaded)}
     else
       :error
     end
   end
 
-  defp load_csv_files([], sheets), do: sheets
-  defp load_csv_files([file | rest], sheets) do
+  defp load_csv_files([], worksheets), do: worksheets
+  defp load_csv_files([file | rest], worksheets) do
     name = Path.basename(file, ".csv")
     csv = File.read! file
-    load_csv_files rest, [WorkSheetData.new(name, file, csv) | sheets]
+    load_csv_files rest, [WorkSheetData.new(name, file, csv) | worksheets]
   end
 
 end
