fc6d062ed67c7e21a1c4a2044d60febba8db7b40
jannekai <janne.kaistinen@iki.fi>
2015-04-13 01:35:47 +0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Googlesheets.Mixfile do
   def project do
     [
       app: :google_sheets,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.0.0",
       description: description,
       package: package,
