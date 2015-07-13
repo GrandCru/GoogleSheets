345bd83543eb02118958c31875d57af61dccbb2c
Janne Kaistinen <janne.kaistinen@iki.fi>
2015-07-14 00:39:40 +0300

    Fix updater test to work with new API

--- test/updater_test.exs
+++ test/updater_test.exs
@@ -1,18 +1,8 @@
-defmodule MockCallbackHandler do
-
+defmodule UpdaterTestCallback do
   @behaviour GoogleSheets.Callback
-
   def on_loaded(id, data) do
-    send :callback_test, {:loaded, id}
+    send :updater_test_process, {:on_loaded, id}
     data
-  end
-
-  def on_saved(id, _data) do
-    send :callback_test, {:saved, id}
-  end
-
-  def on_unchanged(id) do
-    send :callback_test, {:unchanged, id}
   end
 end
 
@@ -22,25 +12,27 @@ defmodule UpdaterTest do
   require Logger
 
   test "Test updater process" do
-    Process.register self, :callback_test
+    Process.register self, :updater_test_process
 
     cfg = [
-      id: :callback_spreadsheet,
+      id: :updater_test_spreadsheet,
       sheets: ["KeyValue"],
+      callback: UpdaterTestCallback,
+      loader: GoogleSheets.Loader.Docs,
       poll_delay_seconds: 1,
-      callback_module: MockCallbackHandler,
-      loader_init: [module: GoogleSheets.Loader.FileSystem, dir: "priv/data"],
-      loader_poll: [module: GoogleSheets.Loader.Docs, url: "https://spreadsheets.google.com/feeds/worksheets/1k-N20RmT62RyocEu4-MIJm11DZqlZrzV89fGIddDzIs/public/basic"]
+      dir: "priv/data",
+      url: "https://spreadsheets.google.com/feeds/worksheets/1k-N20RmT62RyocEu4-MIJm11DZqlZrzV89fGIddDzIs/public/basic"
     ]
+
+    # Assert data has been loaded,
+    # first by file system loader in the init phase of updater process
+    # and immediately after by configure loader module.
     {:ok, _updater_pid} = GoogleSheets.Updater.start_link(cfg)
+    assert_receive {:on_loaded, :updater_test_spreadsheet}, 120_000
+    assert_receive {:on_loaded, :updater_test_spreadsheet}, 120_00
 
-    assert_receive {:loaded, :callback_spreadsheet}, 120_000
-    assert_receive {:saved, :callback_spreadsheet}, 120_000
-    assert_receive {:unchanged, :callback_spreadsheet}, 120_000
+    # Trigger manual update
+    assert {:ok, _msg} = GoogleSheets.update :updater_test_spreadsheet
   end
 
-  test "Test manual update request" do
-    assert {:ok, result} = GoogleSheets.update_config :single_worksheet
-    Logger.debug "Received update result #{inspect result}"
-  end
 end
