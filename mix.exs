1713162a804f69edbfa047b06a7364fe1bd11597
jannekai <janne.kaistinen@iki.fi>
2017-11-24 20:05:31 +0200

    Update dependencies and version to 2.0.11

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GoogleSheets.Mixfile do
   def project do
     [
       app: :google_sheets,
-      version: "2.0.10",
+      version: "2.0.11",
       elixir: "~> 1.2",
       description: description(),
       package: package(),
